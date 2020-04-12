8009cb54e1abe560a2bc0b29a8729eb11c7a073c
youju <youju@DESKTOP-FF3HUVI>
2020-04-12 18:22:41 +0900

    . 하나 빼기

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 		public static void main(String[] args) {
 			System.out.println("Hi bro What's up?");
 			System.out.println("I'm good bro");
-			System.out.println("bye bro..");
+			System.out.println("bye bro.");
 		}
 }
